cbc42649111ec8cb8b7ccfb540942ff51b035381
Bolun Zhao <bzhao325@gatech.edu>
2023-08-15 16:56:46 -0700

    Project started

---
+++ src/main/java/com/blogproject/springbootblogrestapi/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.blogproject.springbootblogrestapi.repository;
+
+import com.blogproject.springbootblogrestapi.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
