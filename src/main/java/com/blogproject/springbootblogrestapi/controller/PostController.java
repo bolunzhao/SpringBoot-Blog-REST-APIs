6a9c721ccead201f1bb6ec8b1374d71fb91fdfcf
Bolun Zhao <bzhao325@gatech.edu>
2023-08-16 10:39:18 -0700

    Post function added

---
+++ src/main/java/com/blogproject/springbootblogrestapi/controller/PostController.java
@@ -0,0 +1,62 @@
+package com.blogproject.springbootblogrestapi.controller;
+
+import com.blogproject.springbootblogrestapi.payload.PostDto;
+import com.blogproject.springbootblogrestapi.payload.PostResponse;
+import com.blogproject.springbootblogrestapi.service.PostService;
+import com.blogproject.springbootblogrestapi.utils.AppConstants;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post REST API
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // get all posts REST API
+    @GetMapping
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ) {
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    // get post by id REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+    // update post by id REST API
+    @PutMapping("/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+
+        PostDto postResponse = postService.updatePost(postDto, id);
+
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+
+    // delete post REST API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id) {
+
+        postService.deletePostById(id);
+
+        return new ResponseEntity<>("Post entity deleted successfully", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/blogproject/springbootblogrestapi/service/PostService.java
@@ -0,0 +1,18 @@
+package com.blogproject.springbootblogrestapi.service;
+
+import com.blogproject.springbootblogrestapi.payload.PostDto;
+import com.blogproject.springbootblogrestapi.payload.PostResponse;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
+
+    PostDto getPostById(long id);
+
+    PostDto updatePost(PostDto postDto, long id);
+
+    void deletePostById(long id);
+}
