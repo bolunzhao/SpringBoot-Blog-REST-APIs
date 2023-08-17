caa6422a4255f7bc7bb44c047f62e4b2811f2eb3
Bolun Zhao <bzhao325@gatech.edu>
2023-08-16 17:53:40 -0700

    Validation enforced

--- src/main/java/com/blogproject/springbootblogrestapi/controller/PostController.java
+++ src/main/java/com/blogproject/springbootblogrestapi/controller/PostController.java
@@ -4,6 +4,7 @@ import com.blogproject.springbootblogrestapi.payload.PostDto;
 import com.blogproject.springbootblogrestapi.payload.PostResponse;
 import com.blogproject.springbootblogrestapi.service.PostService;
 import com.blogproject.springbootblogrestapi.utils.AppConstants;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class PostController {
 
     // create blog post REST API
     @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+    public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
@@ -44,7 +45,7 @@ public class PostController {
 
     // update post by id REST API
     @PutMapping("/{id}")
-    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+    public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
 
         PostDto postResponse = postService.updatePost(postDto, id);
 
